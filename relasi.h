2f08e590b311f3050ae0f4a62df30453826cce35
aldia1225 <aldi8247@gmail.com>
2025-12-17 12:01:36 +0700

    first

--- relasi.h
+++ relasi.h
@@ -26,5 +26,10 @@ void insertAfterRelasi(List_relasi &L, addr_relasi p, addr_relasi prec);
 void deleteFirstRelasi(List_relasi &L, addr_relasi &p);
 void deleteLastRelasi(List_relasi &L, addr_relasi &p);
 void deleteAfterRelasi(List_relasi &L, addr_relasi p, addr_relasi prec);
-
+void showChildFromParent(List_relasi LR, List_kurir LK, int IDKurir);
+void showAllParentWithChild(List_relasi LR, List_kurir LK);
+void showAllChildWithParent(List_relasi LR, List_paket LP);
+void showParentFromChild(List_relasi LR, List_paket LP, int IDPaket);
+void editRelasi(List_relasi &LR, List_kurir LK, List_paket LP, int IDKurir, int IDPaketLama, int IDPaketBaru);
+void autoInputRelasi(List_relasi &LR, List_kurir &LK, List_paket LP);
 #endif // RELASI_H_INCLUDED
