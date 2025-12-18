df8aab469d6b930fbeb508a24ec9264c8d6d01d0
aldia1225 <aldi8247@gmail.com>
2025-12-18 21:26:10 +0700

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "menu.h"
-
-int main() {
-    List_kurir LK; createListKurir(LK);
-    List_paket LP; createListPaket(LP);
-    List_relasi LR; createListRelasi(LR);
-    menuUtama(LK, LP, LR);
-    return 0;
-}
